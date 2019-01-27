9a819d58c12565b98eb9dca13e9b49914c263dc1
Kuijmont <fkmontero9@gmail.com>
2019-01-27 10:40:11 +0100

    ñ

--- GestorBD/src/gestorbd/jFrameClientes.java
+++ GestorBD/src/gestorbd/jFrameClientes.java
@@ -579,7 +579,11 @@ public class jFrameClientes extends javax.swing.JFrame {
                                         
                                 break;
                             case JOptionPane.CANCEL_OPTION://Si das a "cancelar" en la ventana anterior muestra este mensaje.
+
                                 resetFields2();
+
+                              
+
                                 fieldCodigo.grabFocus();
                                 break;
                             case JOptionPane.CLOSED_OPTION://Si cierras la ventana anterior muestra este mensaje.
